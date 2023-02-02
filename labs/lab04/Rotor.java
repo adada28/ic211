828b980db2b7813e92d9c6fe3a1aa27960f49a1f
adada28 <m251224@usna.edu>
2023-02-02 15:15:58 -0500

    msg

---
+++ labs/lab04/Rotor.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Rotor{
+    private String alphabet;
+    private char topChar;
+
+    public Rotor(String m, char top){
+        this.alphabet = alpha; 
+        this.topChar = top;
+
+        while(this.topChar != this.alphabet[0]){
+            rotate()
+        }
+    }
+
+    public void rotate(){
+
+    }
+
+    public int findIndex(char c, String s){
+        return s.indexOf(c);
+    }
+
+    public char findChar(int index, String s){
+        return s.charAt(index);
+    }
+}
